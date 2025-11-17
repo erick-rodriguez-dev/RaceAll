4694111495702a99c5830dc70f02267d2e42edca
Axl-Rodriguez-dev <erick.rodriguez1952@gmail.com>
2025-11-16 21:29:15 -0500

    IGNORE THIS COMMIT - (rename folders)

--- Broadcast/Structs/connection_state.go
+++
@@ -1,9 +0,0 @@
-package structs
-
-// ConnectionState represents the state of a broadcast connection
-type ConnectionState struct {
-	ConnectionId      int
-	ConnectionSuccess bool
-	IsReadonly        bool
-	Error             string
-}
